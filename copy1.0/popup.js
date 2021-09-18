ca4f13b2268e4cfa7b1558a94318157c43a230e5
zhipeng.lin <zhipeng.lin@zstack.io>
2021-09-18 15:35:55 +0800

    fix: csdn extra text

--- copy1.0/popup.js
+++ copy1.0/popup.js
@@ -1,33 +1,41 @@
+const doCopy = () => {
+  const setUserSelect = (e) => {
+    let current = e.target
+    while(current.parentNode && current.nodeName !== 'DIV' && current !== document.body){
+      current.style.userSelect = 'auto'
+      current = current.parentNode
+      console.log('set user select')
+    }
+    document.removeEventListener('click', setUserSelect)
+  }
 
-window.onload = () => {
+  const isChosen = window.getSelection ? window.getSelection().type === 'Range' : document.selection.createRange().text
+  ;(window.getSelection && console.log(window.getSelection()))
+  if(isChosen){
+    document.execCommand('copy')
+  } else {
+   document.addEventListener('click', setUserSelect)
+  }
+}
+
+window.onload = async () => {
   let copyButton = document.getElementById("copy-text")
   
-  copyButton.addEventListener('click', async () => {
-    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true })
-    const doFnInPage = fn => chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      function: fn,
-    })
-    
-    const doCopy = () => {
-      const isChosen = window.getSelection ? window.getSelection().type === 'Range' : document.selection.createRange().text
-      ;(window.getSelection && console.log(window.getSelection()))
-      if(isChosen){
-        document.execCommand('copy')
-      } else {
-        const setUserSelect = (e) => {
-          let current = e.target
-          while(current.parentNode && current.nodeName !== 'DIV' && current !== document.body){
-            current.style.userSelect = 'auto'
-            current = current.parentNode
-            console.log('set user select')
-          }
-          document.removeEventListener('click', setUserSelect)
-        }
-       document.addEventListener('click', setUserSelect)
-      }
-    }
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true })
+  const doFnInPage = fn => chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: fn,
+  })
 
+  doFnInPage(() => {
+    // remove extra text
+    document.addEventListener('copy', (event) => {
+      event.clipboardData.setData('text', document.getSelection())
+      event.preventDefault()
+    })
+  })
+
+  copyButton.addEventListener('click', () => {
     doFnInPage(doCopy)
   })
 }
